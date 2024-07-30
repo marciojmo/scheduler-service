645bda31fbb832391e13f5f2c7b9aab62e64585c
Marcio <marciomjmo@gmail.com>
2024-07-30 16:02:14 -0300

    Add swagger documentation and tags on app.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Scheduler API')
+    .setDescription('An API for handling schedules and tasks.')
+    .setVersion('1.0')
+    .addTag('schedules')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
